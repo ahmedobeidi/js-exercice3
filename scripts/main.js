85bd5138994d5663c1b0e69a9c6f72b2fd46083d
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-14 11:09:38 +0100

    DONE: Exercise 3

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,22 @@
 /* Exercice 1 
-let stars = "";
-for (let i = 0; i < 5; i++) {
-    stars += "*"
-    console.log(stars);
+let iterations = 5;
+let numberOfSpaces = 4;
+
+for (let i = 1; i <= iterations; i++) {
+
+let star = "";
+let space = "";
+
+for (let j = 0; j < numberOfSpaces; j++) {
+    space += " ";
+}
+
+for (let x = 0; x < i; x++) {
+    star += "* "
+}
+
+console.log(space, star);
+numberOfSpaces--;
 }
 */
 
@@ -12,4 +26,13 @@ const superHeros = ["Spiderman", "Captain America", "Wolverine", "Superman"];
 superHeros.forEach(function(hero) {
     console.log(`Je suis fan de ${hero}`);
 });
+*/
+
+/* Exercise 3 
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+    if (i === 0) { 
+        console.log("Décollage");
+    }
+}
 */
